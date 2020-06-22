96849dbbdb1e72385c365ceab25fed4fe46d90b8
nom30523 <arikatyo-saiko-@hotmail.co.jp>
2020-06-22 19:57:12 +0900

    post indexビュー 一覧表示

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">ニックネーム</th>
+        <th scope="col">タイトル</th>
+        <th scope="col">投稿日時</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($posts as $post)
+        <tr>
+          <th scope="row">{{ $post->id }}</th>
+          <td>{{ $post->user->name }}</td>
+          <td><a href="#">{{ $post->title }}</a></td>
+          <td>{{ $post->created_at }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
+@endsection
