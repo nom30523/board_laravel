317378e0a67eca7a4271e244c039b1d13895cebb
nom30523 <arikatyo-saiko-@hotmail.co.jp>
2020-06-22 19:55:11 +0900

    新規投稿へのリンク追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{ route('post.create') }}" class="dropdown-item">新規投稿</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
