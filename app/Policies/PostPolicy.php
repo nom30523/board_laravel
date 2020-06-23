48fc57e55981f95d38e633d401aad4f3f21bc698
nom30523 <arikatyo-saiko-@hotmail.co.jp>
2020-06-23 17:18:34 +0900

    postポリシーで投稿ユーザーのみ編集削除ができるように設定

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Auth;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function update(User $user, Post $post)
+    {
+        return $user->id === $post->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        return $user->id === $post->user_id;
+    }
+
+}
